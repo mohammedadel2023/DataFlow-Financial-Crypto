a556ed741203972fe7f681be687ac921832e7f82
mohammed adel <mohammedkhashan2005@gmail.com>
2026-03-10 00:22:37 +0300

    write first producer and add requred lib to the req file

---
+++ trial/first_kafka_producer.py
@@ -0,0 +1,16 @@
+from confluent_kafka import Producer 
+
+
+my_producer = Producer({
+    'bootstrap.servers': 'localhost:9094'
+})
+
+def delivery_report(err, msg):
+    if err:
+        print(f"Delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+        print("all done")
+
+my_producer.produce('test_topic', value='Hello World', callback=delivery_report)
+my_producer.flush()
