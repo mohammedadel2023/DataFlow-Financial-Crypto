e603a4955e3ed2e2628d0370874133a84b368025
mohammed adel <mohammedkhashan2005@gmail.com>
2026-04-30 15:53:52 +0300

    saving before editing to fix the blocking issue from coindesc

--- trial/first_kafka_producer.py
+++ trial/first_kafka_producer.py
@@ -12,5 +12,5 @@ def delivery_report(err, msg):
         print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
         print("all done")
 
-my_producer.produce('test_topic', value='Hello World', callback=delivery_report)
+my_producer.produce('test_topic', value='Hello World'.encode('utf-8'), callback=delivery_report)
 my_producer.flush()
